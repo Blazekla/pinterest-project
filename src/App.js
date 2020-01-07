d8624aff35bdef2885eb920ee49d24c427565e78
Blazekla <cristlui4@gmail.com>
2020-01-07 07:35:34 -0600

    Adding initial code for Pinterest project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "typeface-roboto";
+// import Typography from "@material-ui/core/Typography";
+import PrimarySearchAppBar from "./components/AppBar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import ImageGridList from "./components/ImageGridList";
+import Container from "@material-ui/core/Container";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <CssBaseline>
+        <div className="App">
+          <PrimarySearchAppBar />
+
+          <Container maxWidth="lg">
+            <ImageGridList />
+          </Container>
+        </div>
+      </CssBaseline>
+    </React.Fragment>
   );
 }
 
---
+++ src/images/tileData.js
@@ -0,0 +1,78 @@
+import seal1 from "./seal1.jpg";
+import seal2 from "./seal2.jpg";
+import babyseal from "./babyseal.png";
+import photogenicseal from "./photogenicseal.jpg";
+
+const tileData = [
+  {
+    img: babyseal,
+    title: "babyseal",
+    author: "seal's mom",
+    cols: 9
+    // featured: true
+  },
+  {
+    img: photogenicseal,
+    title: "model seal",
+    author: "seal agency"
+  },
+  {
+    img: seal1,
+    title: "seal selfie",
+    author: "seal momma"
+  },
+  {
+    img: seal2,
+    title: "cute seal",
+    author: "sealpup"
+    // featured: true
+  }
+  // {
+  //   img: babyseal,
+  //   title: "babyseal",
+  //   author: "seal's mom",
+  //   cols: 9
+  //   // featured: true
+  // },
+  // {
+  //   img: photogenicseal,
+  //   title: "model seal",
+  //   author: "seal agency"
+  // },
+  // {
+  //   img: seal1,
+  //   title: "seal selfie",
+  //   author: "seal momma"
+  // },
+  // {
+  //   img: seal2,
+  //   title: "cute seal",
+  //   author: "sealpup"
+  //   // featured: true
+  // },
+  // {
+  //   img: babyseal,
+  //   title: "babyseal",
+  //   author: "seal's mom",
+  //   cols: 9
+  //   // featured: true
+  // },
+  // {
+  //   img: photogenicseal,
+  //   title: "model seal",
+  //   author: "seal agency"
+  // },
+  // {
+  //   img: seal1,
+  //   title: "seal selfie",
+  //   author: "seal momma"
+  // },
+  // {
+  //   img: seal2,
+  //   title: "cute seal",
+  //   author: "sealpup"
+  //   // featured: true
+  // }
+];
+
+export default tileData;
