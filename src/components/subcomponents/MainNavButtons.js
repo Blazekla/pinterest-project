c780c4f16989f7739dbceb03df51b065098bdc7e
Luis Cristo <cristlui4@gmail.com>
2020-05-01 15:25:12 -0500

    added responsive button initial code

--- src/components/subcomponents/MainNavButtons.js
+++ src/components/subcomponents/MainNavButtons.js
@@ -4,6 +4,7 @@ import { NavLink } from "react-router-dom";
 //Import MaterialUI Components
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles((theme) => ({
   sectionDesktop: {
@@ -11,13 +12,15 @@ const useStyles = makeStyles((theme) => ({
     [theme.breakpoints.up("md")]: {
       display: "flex",
       alignItems: "center",
+      width: "auto",
     },
   },
   sectionMobile: {
-    //   display: "flex",
-    //   [theme.breakpoints.up("md")]: {
-    //     display: "none",
-    //   },
+    display: "flex",
+    width: "auto",
+    [theme.breakpoints.up("md")]: {
+      display: "none",
+    },
   },
   buttonBackground: {
     backgroundColor: "#111",
@@ -34,27 +37,32 @@ const useStyles = makeStyles((theme) => ({
 function MainNavButtons() {
   const classes = useStyles();
   return (
-    <div className={classes.sectionDesktop}>
-      <Button
-        style={{ color: "#333" }}
-        exact
-        component={NavLink}
-        to="/"
-        className={classes.test}
-        activeClassName={classes.buttonBackground}
-      >
-        Home
-      </Button>
+    <React.Fragment>
+      <Grid container className={classes.sectionDesktop}>
+        <Button
+          style={{ color: "#333" }}
+          exact
+          component={NavLink}
+          to="/"
+          className={classes.test}
+          activeClassName={classes.buttonBackground}
+        >
+          Home
+        </Button>
 
-      <Button
-        component={NavLink}
-        to="/following"
-        style={{ color: "#8e8e8e" }}
-        activeClassName={classes.buttonBackground}
-      >
-        Following
-      </Button>
-    </div>
+        <Button
+          component={NavLink}
+          to="/following"
+          style={{ color: "#8e8e8e" }}
+          activeClassName={classes.buttonBackground}
+        >
+          Following
+        </Button>
+      </Grid>
+      <Grid container className={classes.sectionMobile}>
+        <Button>Mobile!</Button>
+      </Grid>
+    </React.Fragment>
   );
 }
 
